614d426e8d7c640cb1709ef3fbdf1c2e6304bc02
danielwalke <daniel.walke@ovgu.de>
2021-07-05 15:08:24 +0200

    -changed opacity to 20%

--- keggcalculator-frontend/src/Creator/graph/Constants.js
+++ keggcalculator-frontend/src/Creator/graph/Constants.js
@@ -1,7 +1,7 @@
 export const COMPOUND_NODE_COLOR = "#FF8000"
 export const COMPOUND_NODE_SYMBOL = "circle"
 export const KEY_COMPOUND_OPACITY = 1
-export const NOT_KEY_COMPOUND_OPACITY = 0.1
+export const NOT_KEY_COMPOUND_OPACITY = 0.2
 export const REACTION_NODE_SIZE = 200
 export const REACTION_NODE_COLOR = "black"
 export const REACTION_NODE_SYMBOL = "diamond"
