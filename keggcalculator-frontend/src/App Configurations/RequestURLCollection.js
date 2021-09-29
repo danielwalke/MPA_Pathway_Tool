e469797370c4d3111e85e093000e3e9be1729c4d
Abdullahil Kafi <kafiabdullahil96@gmail.com>
2021-09-29 13:15:27 +0200

    Flux Visualization Updates

--- keggcalculator-frontend/src/App Configurations/RequestURLCollection.js
+++ keggcalculator-frontend/src/App Configurations/RequestURLCollection.js
@@ -12,33 +12,31 @@ export const endpoint_download_unmatched_proteins = host + ":" + portNumber + "/
 
 export const endpoint_download = host + ":" + portNumber + "/keggcalculator/download";
 
-export const endpoint_download_details = host + ":" + portNumber + "/keggcalculator/downloadDetails";
-
 export const endpoint_getCompoundList = host + ":" + portNumber + "/keggcreator/compoundlist";
 
-export const endpoint_getReactionAndProduct = host + ":" + portNumber + "/keggcreator/reactiondatabysubstrate";
+export const endpoint_getReactionAndProduct =  host + ":" + portNumber + "/keggcreator/reactiondatabysubstrate";
 
-export const endpoint_getTaxonomyIdList = host + ":" + portNumber + "/keggcreator/taxonomyIdList";
+export const endpoint_getTaxonomyIdList =  host + ":" + portNumber + "/keggcreator/taxonomyIdList";
 
-export const endpoint_getReactionUrl = host + ":" + portNumber + "/keggcreator/getreaction";
+export const endpoint_getReactionUrl =  host + ":" + portNumber + "/keggcreator/getreaction";
 
-export const endpoint_getReactionsByEcList = host + ":" + portNumber + "/keggcreator/getreactionlistbyeclist";
+export const endpoint_getReactionsByEcList =  host + ":" + portNumber + "/keggcreator/getreactionlistbyeclist";
 
-export const endpoint_getReactionsByKoList = host + ":" + portNumber + "/keggcreator/getreactionlistbykolist";
+export const endpoint_getReactionsByKoList =  host + ":" + portNumber + "/keggcreator/getreactionlistbykolist";
 
-export const endpoint_getModule = host + ":" + portNumber + "/keggcreator/module";
+export const endpoint_getModule =  host + ":" + portNumber + "/keggcreator/module";
 
-export const endpoint_getReactionsBySubstrate = host + ":" + portNumber + "/keggcreator/reactiondatabysubstrate";
+export const endpoint_getReactionsBySubstrate =  host + ":" + portNumber + "/keggcreator/reactiondatabysubstrate";
 
-export const endpoint_getModuleList = host + ":" + portNumber + "/keggcreator/modulelist";
+export const endpoint_getModuleList =  host + ":" + portNumber + "/keggcreator/modulelist";
 
-export const endpoint_getEcNumberList = host + ":" + portNumber + "/keggcreator/ecnumberlist";
+export const endpoint_getEcNumberList =  host + ":" + portNumber + "/keggcreator/ecnumberlist";
 
-export const endpoint_getKoNumberList = host + ":" + portNumber + "/keggcreator/konumberlist";
+export const endpoint_getKoNumberList =  host + ":" + portNumber + "/keggcreator/konumberlist";
 
-export const endpoint_getReactionList = host + ":" + portNumber + "/keggcreator/reactions";
+export const endpoint_getReactionList =  host + ":" + portNumber + "/keggcreator/reactions";
 
-export const endpoint_getTaxonomyList = host + ":" + portNumber + "/keggcreator/taxonomylist";
+export const endpoint_getTaxonomyList =  host + ":" + portNumber + "/keggcreator/taxonomylist";
 
 export const endpoint_TaxonomyById = host + ":" + portNumber + "/keggcreator/taxonomy";
 
@@ -46,6 +44,4 @@ export const endpoint_getTaxonomicNames = host + ":" + portNumber + "/keggcreato
 
 export const endpoint_getFilteredTaxonomicNames = host + ":" + portNumber + "/keggcreator/filteredtaxonomicNames";
 
-export const endpoint_getTaxonomicDetails = host + ":" + portNumber + "/keggcalculator/detailedContent";
-
-export const endpoint_getBiggCompoundList = host + ":" + portNumber + "/keggcreator/biggcompoundlist";
+export const endpoint_getFbaSolution = host + ":" + portNumber + "/fluxanalysis/fba";
